ecdc68addbeb47ea8c41db136bdaffb5fefd150d
syafroni megi <syafroni.megi@gmail.com>
2021-03-26 22:03:13 +0700

    Updates

---
+++ precache-manifest.645d20f1bbae8b84d50d5373f241feda.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9cd73172a854d9ff70b57886d515aa",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "85b96999e13c647a4243",
+    "url": "/portofolio/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "ad811654878e0627b545",
+    "url": "/portofolio/static/js/2.f11431b2.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/portofolio/static/js/2.f11431b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b96999e13c647a4243",
+    "url": "/portofolio/static/js/main.e3b2b008.chunk.js"
+  },
+  {
+    "revision": "f121d2b9e6bc6e1ff00d",
+    "url": "/portofolio/static/js/runtime-main.1f57b783.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/portofolio/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/portofolio/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/portofolio/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/portofolio/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/portofolio/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/portofolio/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/portofolio/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/portofolio/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/portofolio/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/portofolio/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/portofolio/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/portofolio/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/portofolio/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/portofolio/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/portofolio/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "ceded99b57a2eed125b185fda22a2550",
+    "url": "/portofolio/static/media/profile-bg.ceded99b.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/portofolio/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/portofolio/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/portofolio/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/portofolio/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/portofolio/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/portofolio/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/portofolio/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "6a0798d7c856be0bace7cddb38ea6af5",
+    "url": "/portofolio/static/media/syafroni.6a0798d7.jpg"
+  }
+]);
